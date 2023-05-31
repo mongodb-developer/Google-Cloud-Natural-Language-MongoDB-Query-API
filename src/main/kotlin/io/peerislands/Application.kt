e56a15907a4f20bbdbef1f6847ef259dcf4e7f02
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2023-05-31 15:14:54 -0500

    :bug: CORS issue resolved

--- src/main/kotlin/io/peerislands/Application.kt
+++ src/main/kotlin/io/peerislands/Application.kt
@@ -7,14 +7,14 @@ import io.peerislands.plugins.*
 import io.ktor.server.plugins.cors.routing.CORS
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     install(CORS) {
         allowNonSimpleContentTypes = true
-        allowHost("0.0.0.0:3000")
+        allowHost("localhost:3000")
     }
     configureRouting()
 }
